99b285bd45d0a162c63e1dde2d2ad222a64aaeec
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-10 23:12:19 +0530

    ADDED:Check Mobile Number is valid or not

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 NAME_PATTERN="^[A-Z][a-z]{3,}$"
 EMAIL_PATTERN="^[a-zA-z0-9]{3,}+[@][a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
+MOBILE_NUMBER_PATTERN="^[0-9]{2} [0-9]{10,10}$"
 
 function validation()
 {
@@ -12,14 +13,18 @@ function validation()
 	fi
 
 }
-//cheking first name is valid or not
+#cheking first name is valid or not
 read -p "enter first name: " name
 validation $name $NAMEPATTERN
 
-//cheking last name is valid or not
+#cheking last name is valid or not
 read -p "enter last name: " lastName
 validation $lastName $NAMEPATTERN
 
-//checking mailid is valid or not 
+#checking mailid is valid or not 
 read -p "enter email"  email
 validation $email $EMAIL_PATTERN
+
+#checking mobile pattern is valid or not
+read -p "enter mobile number: " mobileNumber
+validation $mobileNumber $MOBILE_NUMBER_PATTERN
