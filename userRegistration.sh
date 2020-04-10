fd7ed8dfbbdfd1dfb56aad77d09ea2cbc3e76faf
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-10 21:41:38 +0530

    ADDED:Ckecking First Name is valid or not

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -x
-echo "Welcome to User Registration"
+NAMEPATTERN="^[A-Z][a-z]{3,}$"
+
+function validation()
+{
+	if [[ $1 =~ $2 ]]
+	then
+   	echo "valid"
+	else
+   	echo "invalid"
+	fi
+
+}
+//cheking first name is valid or not
+read -p "enter first name: " name
+validation $name $NAMEPATTERN
 
