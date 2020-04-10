4da6e28493fa2f3cc3b5151c2e706c30e6508e23
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-10 23:37:09 +0530

    ADDED:Checking Password Contain One UpperCase Letter

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,8 +3,9 @@
 #constant
 NAME_PATTERN="^[A-Z][a-z]{3,}$"
 EMAIL_PATTERN="^[a-zA-z0-9]{3,}+[@][a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
-MOBILE_NUMBER_PATTERN="^[0-9]{2} [0-9]{10,10}$"
+MOBILE_NUMBER_PATTERN="^[0-9]{2}[0-9]{10,10}$"
 PASSWORD_MINIMUM_EIGHT_CHARACTER="^[a-z]{8,}"
+PASSWORD_CONTAIN_AT_LIST_ONE_UPPERCASE_CHARACTER="^[a-zA-Z0-9]*(.*[A-Z].*{1}+)[a-zA-Z0-9]*{7,40}$"
 
 function validation()
 {
@@ -14,7 +15,6 @@ function validation()
 	else
    	echo "invalid"
 	fi
-
 }
 #cheking first name is valid or not
 read -p "enter first name: " name
@@ -36,3 +36,6 @@ validation $mobileNumber $MOBILE_NUMBER_PATTERN
 read -p "enter password:" password
 validation $password $PASSWORD_MINIMUM_EIGHT_CHARACTER
 
+#checking password contain one Uppercase letter
+read -p "enter password:" password
+validation $password $PASSWORD_CONTAIN_AT_LIST_ONE_UPPERCASE_CHARACTER
