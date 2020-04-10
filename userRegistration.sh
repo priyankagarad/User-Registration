acc76ce84b85e8d9bdd267128e24e2d13a398249
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-10 23:26:04 +0530

    ADDED:Check password length is minimum eight character

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
+
+#constant
 NAME_PATTERN="^[A-Z][a-z]{3,}$"
 EMAIL_PATTERN="^[a-zA-z0-9]{3,}+[@][a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
 MOBILE_NUMBER_PATTERN="^[0-9]{2} [0-9]{10,10}$"
+PASSWORD_MINIMUM_EIGHT_CHARACTER="^[a-z]{8,}"
 
 function validation()
 {
@@ -28,3 +31,8 @@ validation $email $EMAIL_PATTERN
 #checking mobile pattern is valid or not
 read -p "enter mobile number: " mobileNumber
 validation $mobileNumber $MOBILE_NUMBER_PATTERN
+
+#checking password length
+read -p "enter password:" password
+validation $password $PASSWORD_MINIMUM_EIGHT_CHARACTER
+
