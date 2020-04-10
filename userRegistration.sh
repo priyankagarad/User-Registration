4da3e678bbe1edc9f287e0ac20b2d07b6db73b72
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-11 00:20:19 +0530

    ADDED:Check email is valid or not

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -x
 #constant
 NAME_PATTERN="^[A-Z][a-z]{3,}$"
-EMAIL_PATTERN="^[a-zA-z0-9]{3,}+[@][a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
 MOBILE_NUMBER_PATTERN="^[0-9]{2}[0-9]{10,10}$"
+
+#password pattern
 PASSWORD_MINIMUM_EIGHT_CHARACTER="^[a-z]{8,}"
 PASSWORD_CONTAIN_AT_LIST_ONE_UPPERCASE_CHARACTER="^[a-zA-Z0-9]*(.*[A-Z].*{1}+)[a-zA-Z0-9]*{7,40}$"
 PASSWORD_CONTAIN_AT_LIST_ONE_NUMERIC_NUMBER="^[a-zA-Z0-9]*(.*[A-Z].*{1}+)(.*[0-9].*{1}+)[a-zA-Z0-9]*{7,40}$"
 VALID_PASSWORD="^[A-Za-z]{8,}*(.*[0-9].*{1}+)*(.*[A-Z].*{1}+)*(.*[*!@#$%^&=+].*{1})*[a-zA-Z0-9]$"
+
+#email pattern
+EMAIL_PATTERN1="^[a-zA-z0-9]{3,}+[@]{1}"
+EMAIL_PATTERN2="^[a-zA-z0-9]{3,}+[@]{1}[a-zA-z0-9]{3,}"
+EMAIL_PATTERN3="^[a-zA-z0-9]{3,}+[@]{1}[a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
+VALID_EMAIL="^[a-zA-z0-9]{3,}+[@][a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
 
 function validation()
 {
@@ -19,15 +26,15 @@ function validation()
 }
 #cheking first name is valid or not
 read -p "enter first name: " name
-validation $name $NAMEPATTERN
+validation $name $NAME_PATTERN
 
 #cheking last name is valid or not
 read -p "enter last name: " lastName
-validation $lastName $NAMEPATTERN
+validation $lastName $NAME_PATTERN
 
 #checking mailid is valid or not 
 read -p "enter email"  email
-validation $email $EMAIL_PATTERN
+validation $email $VALID_EMAIL
 
 #checking mobile pattern is valid or not
 read -p "enter mobile number: " mobileNumber
