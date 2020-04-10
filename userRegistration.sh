c716af6ec0660e937aa3f84ddb69cbc5aec9f296
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-11 00:05:58 +0530

    ADDED:Check Password Contain one Special Character

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
-
 #constant
 NAME_PATTERN="^[A-Z][a-z]{3,}$"
 EMAIL_PATTERN="^[a-zA-z0-9]{3,}+[@][a-zA-z0-9]{3,}+[.][a-ZA-Z]{2,4}$"
 MOBILE_NUMBER_PATTERN="^[0-9]{2}[0-9]{10,10}$"
 PASSWORD_MINIMUM_EIGHT_CHARACTER="^[a-z]{8,}"
 PASSWORD_CONTAIN_AT_LIST_ONE_UPPERCASE_CHARACTER="^[a-zA-Z0-9]*(.*[A-Z].*{1}+)[a-zA-Z0-9]*{7,40}$"
+PASSWORD_CONTAIN_AT_LIST_ONE_NUMERIC_NUMBER="^[a-zA-Z0-9]*(.*[A-Z].*{1}+)(.*[0-9].*{1}+)[a-zA-Z0-9]*{7,40}$"
+VALID_PASSWORD="^[A-Za-z]{8,}*(.*[0-9].*{1}+)*(.*[A-Z].*{1}+)*(.*[*!@#$%^&=+].*{1})*[a-zA-Z0-9]$"
 
 function validation()
 {
@@ -39,3 +40,12 @@ validation $password $PASSWORD_MINIMUM_EIGHT_CHARACTER
 #checking password contain one Uppercase letter
 read -p "enter password:" password
 validation $password $PASSWORD_CONTAIN_AT_LIST_ONE_UPPERCASE_CHARACTER
+
+##checking password contain one numeric number
+read -p "enter password:" password
+validation $password $PASSWORD_CONTAIN_AT_LIST_ONE_NUMERIC_NUMBER
+
+#validate password
+read -p "enter password:" password
+validation $password $VALID_PASSWORD
+
